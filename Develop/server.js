4e1e1cb36e7eed6d44d27193015cf4d50db0693d
CyndiCodes <huertascyn@gmail.com>
2023-08-24 19:19:09 -0400

    all get and post requests working

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const PORT = 3001;
 const app = express();
+const db = require('./db/db.json')
+const uuid = require('./helpers/uuid')
+
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,16 +15,45 @@ app.use(express.static('public'));
 
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
-)
+);
 
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-)
+app.get('/api/notes', (req, res) => {
+  res.json(db);
+  
+});
 
-app.get('api/notes', (req, res) => res.json('/db/db.json'))
+app.post('/api/notes', (req, res) => {
+  const { title, text } = req.body;
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid()
+      }
+      
+      db.push(newNote)
+      
+      const noteString = JSON.stringify(db, null, 2);
+      
+      fs.writeFile(`./db/db.json`, noteString, (err) => 
+      err
+      ? console.error(err)
+      : console.log(`${newNote.title}`));
+      
+      const response = {
+        status: 'success',
+        body: newNote
+      };
+      
+      console.log(response);
+      res.status(201).json(response);
+    } 
+    app.get('*', (req, res) =>
+        res.sendFile(path.join(__dirname, '/public/index.html'))
+    );
+  });
 
-app.post('api/notes', (req, res) => res.json('/db/db.json'))
-
-app.listen(PORT, () =>
+  
+  app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
